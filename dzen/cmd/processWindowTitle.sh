ff71335829a766f625e4d4e86db8cbcc24bc5b0b
Felix Crux <felixc@felixcrux.com>
2012-10-27 18:03:42 -0400

    Simplifying the window title processing script for xmonad.

--- dzen/cmd/processWindowTitle.sh
+++ dzen/cmd/processWindowTitle.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/zsh
 
+command -v xprop wmctrl > /dev/null 2>&1 || {
+  while read -e; do; done
+}
+
 while read LINE; do
-  ACTIVE_WINDOW=$(xprop -id $(xprop -root | awk '/^_NET_ACTIVE_WINDOW/{print $5}') | awk '/^WM_NAME/{print $3}' | tr -d '"')
-  INACTIVE_WINDOWS=""
-  OLDIFS=$IFS  # Input Field Separator (saving state to restore after using custom one)
-  IFS=$'\n'
-  for WIN in $(wmctrl -l | awk "/[[:alnum:]]+ +$(wmctrl -d | awk '/*/{print $1}')/{print \$1, \$4}"); do
-    IFS=" "
-    if [[ ${WIN[(w)2,-1]} != ${ACTIVE_WINDOW} ]]; then
-      INACTIVE_WINDOWS="$INACTIVE_WINDOWS [^ca(1,wmctrl -ia $WIN[(w)1]) ${WIN[(w)2,-1]} ^ca()] "
-    fi
-  done
-  IFS=$OLDIFS
-  print " $(echo "$LINE" $INACTIVE_WINDOWS | tr -d '\n')"
+  ACTIVE_WINDOW=$(printf "%#010x\n" $(xprop -root | sed -n 's/^_NET_ACTIVE_WINDOW.*0x\(.*\)/0x0\1/p'))
+  INACTIVE_WINDOWS=$(wmctrl -l | sed -n -e "/^$ACTIVE_WINDOW/d" -e "s/^0x.*  $(wmctrl -d | awk '/*/{print $1}') \w* \(.*\)$/[ \1 ]/p" | tr '\n' ' ')
+  echo "$LINE $INACTIVE_WINDOWS"
 done
