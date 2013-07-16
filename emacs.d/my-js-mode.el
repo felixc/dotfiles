4c962f2775f4cc4f731da1edab7fd6471f9e9755
Felix Crux <felixc@felixcrux.com>
2013-07-16 16:28:18 -0400

    Emacs: In JS2 mode, avoid adding a newline after typing {.
    Closes #8

--- emacs.d/my-js-mode.el
+++ emacs.d/my-js-mode.el
@@ -1,5 +1,14 @@
 (require 'js2-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook '(lambda () (setq fill-column 100)))
 
+(defun prefix-js2-mode-match-curly ()
+  "Call js2-mode's match-curly with a prefix argument to avoid a newline."
+  (interactive)
+  (let ((current-prefix-arg '(1)))
+    (call-interactively 'js2-mode-match-curly)))
+
+(add-hook 'js2-mode-hook '(lambda ()
+  (setq fill-column 100)
+  (setq js2-basic-indent 2)
+  (define-key js2-mode-map (read-kbd-macro "{") 'prefix-js2-mode-match-curly)))
