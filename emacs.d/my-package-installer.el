2f56ac8b9b237fa30f39606836910be26cf09e63
Felix Crux <felixc@felixcrux.com>
2013-08-02 19:33:51 -0400

    Emacs: Add MELPA repository to package archive sources.

--- emacs.d/my-package-installer.el
+++ emacs.d/my-package-installer.el
@@ -2,6 +2,7 @@
 
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (package-initialize)
 
