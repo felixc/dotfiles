9b833aa01511b38410b511802ea7fe36dabda395
Felix Crux <felixc@felixcrux.com>
2015-05-07 18:24:28 -0400

    emacs: Update package management URLs

--- emacs.d/lisp/my-package-installer.el
+++ emacs.d/lisp/my-package-installer.el
@@ -1,8 +1,8 @@
 (require 'package)
 
-(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
 (package-initialize)
 
