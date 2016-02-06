dcd38e693b78b5502b93b6217341836b74c64a5e
Felix Crux <felixc@felixcrux.com>
2016-02-06 14:19:10 -0500

    emacs: Pre-install use-package before requiring it

--- emacs.d/lisp/my-package-management.el
+++ emacs.d/lisp/my-package-management.el
@@ -8,19 +8,19 @@
 
 (package-initialize)
 
-(require 'use-package)
-(setq use-package-always-ensure t)
-
 (defvar my-package-list
   '(ace-jump-mode fill-column-indicator flycheck goto-chg iedit icomplete+
     js2-mode multi-web-mode pabbrev popup rainbow-mode rust-mode typopunct
-    undo-tree yasnippet zencoding-mode zenburn-theme))
+    undo-tree use-package yasnippet zencoding-mode zenburn-theme))
 
 (dolist (package my-package-list)
   (when (not (package-installed-p package))
     (when (not package-archive-contents)
       (package-refresh-contents))
     (package-install package)))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (provide 'my-package-management)
 ;;; my-package-management.el ends here
