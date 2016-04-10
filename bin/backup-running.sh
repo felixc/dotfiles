8e275426225611c332bac814b6cfbd0e21669379
Felix Crux <felixc@felixcrux.com>
2016-04-09 20:11:45 -0400

    New rotating backup system

--- bin/backup-running.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rsync -AXLay --delete --ignore-errors \
-  --exclude .steam --exclude .cache --exclude wrk/git-annex/.git --exclude .cargo \
-  $HOME/ /mnt/backup
---
+++ bin/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -eu
+
+period="${1:-daily}"
+
+dest="/mnt/backup"
+
+/bin/mv $dest/$period-6 $dest/$period-obsolete
+/bin/mv $dest/$period-5 $dest/$period-6
+/bin/mv $dest/$period-4 $dest/$period-5
+/bin/mv $dest/$period-3 $dest/$period-4
+/bin/mv $dest/$period-2 $dest/$period-3
+/bin/mv $dest/$period-1 $dest/$period-2
+
+/usr/bin/rsync \
+  --archive --acls --xattrs --copy-unsafe-links \
+  --fuzzy --link-dest=$dest/$period-2 \
+  --delete-delay --ignore-errors \
+  --exclude-from=$HOME/cfg/backup-exclude \
+  $HOME/ $dest/$period-1
+
+/bin/ln -fsT $dest/$period-1 $dest/latest
+/bin/rm -rf $dest/$period-obsolete
