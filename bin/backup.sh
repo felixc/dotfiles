e4483aa4adefe6882e1ce798ccd910bd6e683c12
Felix Crux <felixc@felixcrux.com>
2016-06-30 20:36:12 -0400

    backup: Quote paths for safety

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,21 +6,21 @@ period="${1:-daily}"
 
 dest="/mnt/backup"
 
-/bin/rm -rf $dest/$period-obsolete
+/bin/rm -rf "$dest/$period-obsolete"
 
-/bin/mv $dest/$period-6 $dest/$period-obsolete
-/bin/mv $dest/$period-5 $dest/$period-6
-/bin/mv $dest/$period-4 $dest/$period-5
-/bin/mv $dest/$period-3 $dest/$period-4
-/bin/mv $dest/$period-2 $dest/$period-3
-/bin/mv $dest/$period-1 $dest/$period-2
+/bin/mv "$dest/$period-6" "$dest/$period-obsolete"
+/bin/mv "$dest/$period-5" "$dest/$period-6"
+/bin/mv "$dest/$period-4" "$dest/$period-5"
+/bin/mv "$dest/$period-3" "$dest/$period-4"
+/bin/mv "$dest/$period-2" "$dest/$period-3"
+/bin/mv "$dest/$period-1" "$dest/$period-2"
 
 /usr/bin/rsync \
   --archive --acls --xattrs --copy-unsafe-links \
-  --fuzzy --link-dest=$dest/$period-2 \
+  --fuzzy --link-dest="$dest/$period-2" \
   --delete-delay --ignore-errors \
-  --exclude-from=$HOME/cfg/backup-exclude \
-  $HOME/ $dest/$period-1
+  --exclude-from="$HOME/cfg/backup-exclude" \
+  "$HOME/" "$dest/$period-1"
 
-/bin/ln -fsT $dest/$period-1 $dest/latest
-/bin/rm -rf $dest/$period-obsolete
+/bin/ln -fsT "$dest/$period-1" "$dest/latest"
+/bin/rm -rf "$dest/$period-obsolete"
