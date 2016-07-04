ff4a9a15791d8491e2dcdbf24070006edbd67313
Felix Crux <felixc@felixcrux.com>
2016-07-04 19:06:45 -0400

    xmonad: Make window title formatter more portable

--- bin/processWindowTitle.sh
+++ bin/processWindowTitle.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/zsh
+#!/bin/sh
 
 command -v xprop wmctrl > /dev/null 2>&1 || {
-  while read -e; do; done
+  while read -r LINE; do echo "$LINE"; done
 }
 
-while read LINE; do
-  ACTIVE_WINDOW=$(printf "%#010x\n" $(xprop -root | sed -n 's/^_NET_ACTIVE_WINDOW.* \(.*\)/\1/p'))
+while read -r LINE; do
+  ACTIVE_WINDOW=$(printf "%#010x\n" "$(xprop -root | sed -n 's/^_NET_ACTIVE_WINDOW.* \(.*\)/\1/p')")
   INACTIVE_WINDOWS=$(wmctrl -l | sed -n -e "/^$ACTIVE_WINDOW/d" -e "s/^0x.*  $(wmctrl -d | awk '/*/{print $1}') \w* \(.*\)$/[ \1 ]/p" | tr '\n' ' ')
   echo " $LINE $INACTIVE_WINDOWS"
 done
