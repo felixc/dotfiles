09f07818f72242825a42ccecca9e67b6991014a8
Felix Crux <felixc@felixcrux.com>
2017-02-03 16:44:03 -0500

    Emacs: Switch Rust config to use use-package

--- emacs.d/lisp/my-rust-config.el
+++ emacs.d/lisp/my-rust-config.el
@@ -2,19 +2,27 @@
 ; $ rustup component add rust-src
 ; $ cargo install racer
 
-(require 'rust-mode)
-
-(setq racer-rust-src-path
-  "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
-
-(add-hook 'rust-mode-hook (lambda ()
-  (setq fill-column 100)
-  (racer-mode)))
-
-(add-hook 'racer-mode-hook (lambda ()
-  (eldoc-mode)
-  (company-mode)))
-
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(use-package rust-mode
+  :mode "\\.rs\\'"
+  :ensure flycheck-rust
+  :init
+  (use-package racer
+    :diminish racer-mode
+    :ensure company
+    :ensure company-racer
+    :config
+    (setq racer-rust-src-path
+      "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src")
+    (add-hook 'racer-mode-hook (lambda ()
+      (eldoc-mode)
+      (company-mode)
+      (diminish 'eldoc-mode))))
+  :config
+  (setq rust-rustfmt-bin "~/.cargo/bin/rustfmt")
+  (add-hook 'rust-mode-hook (lambda ()
+    (racer-mode)
+    (setq fill-column 100)))
+  :bind (:map rust-mode-map
+         ([tab] . company-indent-or-complete-common)))
 
 (provide 'my-rust-config)
