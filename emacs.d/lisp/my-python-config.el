c1c995ba0af0730e8eb975331ecde58aa819c816
Felix Crux <felixc@felixcrux.com>
2017-02-07 18:04:38 -0500

    Emacs: Add autopep8, venv, and use-package features to Python config

--- emacs.d/lisp/my-python-config.el
+++ emacs.d/lisp/my-python-config.el
@@ -1,12 +1,25 @@
 ; Prerequisite configuration:
 ; $ sudo apt-get install python3-jedi pylint3 virtualenv
-; M-x jedi:install-server
 
-(setq flycheck-python-pylint-executable "pylint3")
+; TODO: Automatically enable/disable virtualenvs when entering/leaving files
 
-(add-hook 'python-mode-hook (lambda ()
-  (add-to-list 'company-backends 'company-jedi)
-  (company-mode)
-  (define-key python-mode-map (kbd "TAB") #'company-indent-or-complete-common)))
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python3" . python-mode)
+  :config
+  (use-package company-jedi
+    :config
+    (add-to-list 'company-backends 'company-jedi))
+  (use-package virtualenvwrapper
+    :config
+    (setq venv-location "~/.venv/wrk/"))
+  (use-package py-autopep8
+    :config
+    (setq py-autopep8-options '("--aggressive" "--aggressive"))
+    (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+  (add-hook 'python-mode-hook #'company-mode)
+  (setq flycheck-python-pylint-executable "pylint3")
+  :bind (:map python-mode-map
+          ([tab] . company-indent-or-complete-common)))
 
 (provide 'my-python-config)
