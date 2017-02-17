3c051f8faafdc0de63e484abc3a1757beb233229
Felix Crux <felixc@felixcrux.com>
2017-02-17 17:44:39 -0500

    Emacs: All package management handled by use-package

--- emacs.d/lisp/my-package-management.el
+++ emacs.d/lisp/my-package-management.el
@@ -1,26 +1,25 @@
-;;; my-package-management.el -- Install and manage the packages I want to use
-
 (require 'package)
 
-(setq package-archives '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(setq gnutls-verify-error t)
+
+(setq package-archives
+  '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+    ("marmalade" . "https://marmalade-repo.org/packages/")
+    ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 
-(defvar my-package-list
-  '(ace-jump-mode company fill-column-indicator flycheck goto-chg helm-dash
-    iedit icomplete+ js2-mode multi-web-mode pabbrev rainbow-mode racer
-    rust-mode typopunct undo-tree use-package writeroom-mode yasnippet
-    zenburn-theme))
+(unless (package-installed-p 'use-package)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (package-install 'use-package))
 
-(dolist (package my-package-list)
-  (when (not (package-installed-p package))
-    (when (not package-archive-contents)
-      (package-refresh-contents))
-    (package-install package)))
+(eval-when-compile
+  (require 'use-package))
 
-(require 'use-package)
+(require 'bind-key)
+(require 'diminish)
+
 (setq use-package-always-ensure t)
 
 (provide 'my-package-management)
