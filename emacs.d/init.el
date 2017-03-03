df961b8dc5145d154858639a6c0a3a19d9daea43
Felix Crux <felixc@felixcrux.com>
2017-03-03 11:59:26 -0500

    Emacs: Remove unecessary keybinding for emacsclient

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -124,12 +124,6 @@
 (add-hook 'comint-exec-hook (lambda ()
   (set-process-query-on-exit-flag (get-buffer-process (current-buffer)) nil)))
 
-; Use C-x k to end emacsclient sessions, rather than C-x #
-(add-hook 'server-switch-hook (lambda ()
-  (when (current-local-map)
-    (use-local-map (copy-keymap (current-local-map))))
-  (local-set-key (kbd "C-x k") 'server-edit)))
-
 ; Better undo
 (use-package undo-tree
   :diminish undo-tree-mode
