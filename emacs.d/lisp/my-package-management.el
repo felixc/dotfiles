869ac03d9289f72a82328f23381fcd6352dc99c9
Felix Crux <felixc@felixcrux.com>
2018-09-03 13:11:42 -0400

    Emacs: Auto-update packages

--- emacs.d/lisp/my-package-management.el
+++ emacs.d/lisp/my-package-management.el
@@ -22,4 +22,10 @@
 
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 (provide 'my-package-management)
