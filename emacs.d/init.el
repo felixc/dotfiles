9f16ce4b16fff665255ee963d33e48066a3bd894
Felix Crux <felixc@felixcrux.com>
2019-09-27 13:37:28 -0400

    Emacs: Make fixed-width markdown fonts the right size

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -295,6 +295,7 @@
   (use-package markdown-toc)
   (custom-set-faces
     '(markdown-bold-face ((t (:inherit basic :weight bold))))
+    '(markdown-code-face ((t (:inherit basic))))
     '(markdown-header-delimiter-face ((t (:inherit font-lock-function-name-face :height 1.0 :foreground "dim gray"))))
     '(markdown-header-face ((t (:inherit font-lock-function-name-face :foreground "cadet blue" :weight bold))))
     '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.6))))
