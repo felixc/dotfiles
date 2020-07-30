e2571f62b94a6d5b1e0c5e7cd21e3685734c00e0
Felix Crux <felixc@felixcrux.com>
2020-07-30 17:30:03 -0400

    emacs: Support jsx format in addition to JS

--- emacs.d/lisp/my-webdev-config.el
+++ emacs.d/lisp/my-webdev-config.el
@@ -13,18 +13,12 @@
   (rainbow-mode)
   (setq css-indent-offset 2)))
 
-(use-package js2-mode
+(use-package rjsx-mode
   :mode "\\.js\\'"
   :config
-  (defun prefix-js2-mode-match-curly ()
-    "Call js2-mode's match-curly with a prefix argument to avoid a newline."
-    (interactive)
-    (let ((current-prefix-arg '(1)))
-      (call-interactively 'js2-mode-match-curly)))
-
   (add-hook 'js2-mode-hook '(lambda ()
     (setq fill-column 100)
-    (setq js2-basic-indent 2)
-    (define-key js2-mode-map (read-kbd-macro "{") 'prefix-js2-mode-match-curly))))
+    (setq js-indent-level 2)))
+  (add-hook 'js2-mode-hook 'prettier-js-mode))
 
 (provide 'my-webdev-config)
