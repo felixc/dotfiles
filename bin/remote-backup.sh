ffbadc9cb2ee49d3ecf326faa82d9a4614c47085
Felix Crux <felixc@felixcrux.com>
2022-03-22 19:19:06 -0400

    remote-backup: Variable number of backups to keep by period

--- bin/remote-backup.sh
+++ bin/remote-backup.sh
@@ -2,9 +2,20 @@
 
 set -eux
 
-. $HOME/.keychain/*-sh
+. "$HOME"/.keychain/*-sh
 
 period="${1:-daily}"
+case "$period" in
+    "daily" )
+        backups_to_keep=7 ;;
+    "weekly" )
+        backups_to_keep=4 ;;
+    "monthly" )
+        backups_to_keep=12 ;;
+    * )
+        print "Unrecognized backup time period." 1>&2
+        exit 1 ;;
+esac
 
 dest_host="molniya.felixcrux.com"
 dest_path="/depot/mir"
@@ -20,11 +31,15 @@ dest_path="/depot/mir"
     "$HOME/" "$dest_host:$dest_path/$period-in-progress"
 
 /usr/bin/ssh "$dest_host" /bin/sh << EOF
-  /bin/mv -f "$dest_path/$period-5" "$dest_path/$period-obsolete"
-  /bin/mv "$dest_path/$period-4" "$dest_path/$period-5"
-  /bin/mv "$dest_path/$period-3" "$dest_path/$period-4"
-  /bin/mv "$dest_path/$period-2" "$dest_path/$period-3"
-  /bin/mv "$dest_path/$period-1" "$dest_path/$period-2"
+  set -ux
+  /bin/mv -f "$dest_path/$period-$backups_to_keep" "$dest_path/$period-obsolete"
+  backups_to_keep=$backups_to_keep  # Set the var remotely so we can use it in math
+  offset=1
+  while [ \$offset -lt \$backups_to_keep ]; do
+    /bin/mv "$dest_path/$period-\$((backups_to_keep - offset))" \
+            "$dest_path/$period-\$((backups_to_keep - offset + 1))"
+    offset=\$((offset + 1))
+  done
   /bin/mv "$dest_path/$period-in-progress" "$dest_path/$period-1"
 
   /bin/rm -rf "$dest_path/$period-obsolete"
