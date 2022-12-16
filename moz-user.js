cce7d8281da62209f6136d321844c126ea3f52b6
Felix Crux <felixc@felixcrux.com>
2022-12-15 20:59:58 -0500

    Firefox: Re-enable deprecated compact mode UI

--- moz-user.js
+++ moz-user.js
@@ -81,3 +81,7 @@ user_pref("layout.css.scrollbar-width-thin.disabled", true);
 
 // Load userChrome.css at startup
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Re-enable deprecated "compact mode" UI option
+user_pref("browser.compactmode.show", true);
+user_pref("browser.uidensity", 1);
