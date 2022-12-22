4abcc04240938fc735d474e69d0912ddce9907b5
Felix Crux <felixc@felixcrux.com>
2022-12-21 20:33:11 -0500

    Emacs: Fix 'kill current buffer' functionality

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -103,7 +103,7 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ; We usually want to kill the current buffer.
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-x k") 'kill-current-buffer)
 (global-set-key (kbd "C-x K") 'ido-kill-buffer)
 
 ; Nicer mode line
