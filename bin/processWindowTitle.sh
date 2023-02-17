a78e00239d663515b5b6245c91b5e430b2eedeae
Felix Crux <felixc@felixcrux.com>
2023-02-16 21:14:18 -0500

    Status bar helper: Clean up window title script

--- bin/processWindowTitle.sh
+++ bin/processWindowTitle.sh
@@ -6,6 +6,7 @@ command -v xprop wmctrl > /dev/null 2>&1 || {
 
 while read -r LINE; do
   ACTIVE_WINDOW=$(printf "%#010x\n" "$(xprop -root | sed -n 's/^_NET_ACTIVE_WINDOW.* \(.*\)/\1/p')")
-  INACTIVE_WINDOWS=$(wmctrl -l | sed -n -e "/^$ACTIVE_WINDOW/d" -e "s/^0x.*  $(wmctrl -d | awk '/*/{print $1}') \w* \(.*\)$/[ \1 ]/p" | tr '\n' ' ')
+  ACTIVE_WORKSPACE=$(wmctrl -d | awk '/*/{print $1}')
+  INACTIVE_WINDOWS=$(wmctrl -l | sed -n -e "/^$ACTIVE_WINDOW/d" -e "s/^0x.* $ACTIVE_WORKSPACE \w* \(.*\)$/[ \1 ]/p" | tr '\n' ' ')
   echo " $LINE $INACTIVE_WINDOWS"
 done
